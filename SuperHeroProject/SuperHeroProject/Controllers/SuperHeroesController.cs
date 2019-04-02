4644e7580f1abbfd3a1ab0fa32a45559b85b5164
jarydc333@gmail.com <jarydc333@gmail.com>
2019-04-02 11:04:39 -0500

    Had to redo the whole project again because of a naming error but I have it correct this time and the user can create a superhero and add them to the database

---
+++ SuperHeroProject/SuperHeroProject/Controllers/SuperHeroesController.cs
@@ -0,0 +1,93 @@
+﻿using SuperHeroProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroProject.Controllers
+{
+    public class SuperHeroesController : Controller
+    {
+
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: SuperHeroes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SuperHeroes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperHeroes/Create
+        public ActionResult Create()
+        {
+            SuperHero superHero = new SuperHero();
+            return View(superHero);
+        }
+
+        // POST: SuperHeroes/Create
+        [HttpPost]
+        public ActionResult Create(SuperHero superHero)
+        {
+            try
+            {
+                db.SuperHeroes.Add(superHero);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHeroes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHeroes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHeroes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHeroes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SuperHeroProject/SuperHeroProject/Models/SuperHero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroProject.Models
+{
+    public class SuperHero
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string alterName { get; set; }
+        public string ability { get; set; }
+        public string secondAbility { get; set; }
+        public string catchphrase { get; set; }
+    }
+}
