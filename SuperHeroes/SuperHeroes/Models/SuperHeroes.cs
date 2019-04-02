6de075fb294875453a2989bcac140c46f7392ce3
jarydc333@gmail.com <jarydc333@gmail.com>
2019-04-02 09:49:24 -0500

    Initial commit

---
+++ SuperHeroes/SuperHeroes/Models/SuperHeroes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes.Models
+{
+    public class SuperHeroes
+    {
+        public string name;
+        public string alterEgoName;
+        public string superHeroAbility;
+        public string secondAbility;
+        public string catchphrase;
+
+        public SuperHeroes()
+        {
+                
+        }
+    }
+}
